9526c16f91fd0f3bea7ebe4e2d9b3fa680635a72
Stokerx <94090843+Stokerx@users.noreply.github.com>
2022-09-18 22:38:11 -0500

    Add files via upload

---
+++ semana1/src/seman1/JOoptionPane.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package seman1;
+
+/**
+ *
+ * @author alumno
+ */
+class JOoptionPane {
+
+    static void showMessageDialog(Object object, String usuario_yo_clave_Incorrecto, String senati, int ERROR_MESSAGE) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
